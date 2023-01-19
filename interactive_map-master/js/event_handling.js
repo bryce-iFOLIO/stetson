ee5ba26e28c4c21a0910472338c1d23886634736
bryce-iFOLIO <119603714+bryce-iFOLIO@users.noreply.github.com>
2023-01-19 14:57:19 -0500

    Update event_handling.js

--- interactive_map-master/js/event_handling.js
+++ interactive_map-master/js/event_handling.js
@@ -11,6 +11,20 @@
             if (wd >= 1024) {
                 console.log(shapeName)
                 $(`#card-${shapeName}`).show();
+                
+                const cardset = document.getElementsByClassName("card-left");
+                for (i = 0; i < cardset.length; i++){
+                    if (cardset[i].style.display != "none"){
+                        console.log(i);
+                        const hvalue = document.getElementsByClassName("card-body")[i+1].offsetHeight;
+                        console.log(document.getElementsByClassName("card-body"));
+                        console.log(hvalue);
+                        const thresh = hvalue + 255;
+                        if (thresh >= window.innerHeight * .9){
+                            document.getElementById("gOverlay").style.display = "block";
+                        }
+                    }
+                }
             }
         }
 
